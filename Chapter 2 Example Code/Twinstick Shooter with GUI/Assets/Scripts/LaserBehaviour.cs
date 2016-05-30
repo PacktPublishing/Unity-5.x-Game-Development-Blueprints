cd430a9b5d294f365c2bdd692aa69ed7bcf811af
Vishal Mewada <vishalm.packtpub@gmail.com>
2016-05-30 15:46:45 +0530

    Chapter 2 Code Files added

---
+++ Chapter 2 Example Code/Twinstick Shooter with GUI/Assets/Scripts/LaserBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class LaserBehaviour : MonoBehaviour 
+{
+	// How long the laser will live
+	public float lifetime = 2.0f;
+	
+	// How fast will the laser move
+	public float speed = 5.0f;
+	
+	// How much damage will this laser do if we hit an enemy
+	public int damage = 1;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		// The game object that contains this component will 
+		// be destroyed after lifetime seconds have passed
+		Destroy(gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate(Vector3.up * Time.deltaTime 
+		                    * speed);
+	}
+}
---
+++ Chapter 2 Example Code/Twinstick Shooter with GUI/Assets/Scripts/PauseMenuBehaviour.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+public class PauseMenuBehaviour : MainMenuBehaviour
+{
+    public static bool isPaused;
+
+    public GameObject pauseMenu;
+    public GameObject optionsMenu;
+
+    public void Start()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+
+        UpdateQualityLabel();
+        UpdateVolumeLabel();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp("escape"))
+        {
+            if (!optionsMenu.activeInHierarchy)
+            {
+                // If false becomes true and vice-versa
+                isPaused = !isPaused;
+
+                // If isPaused is true, 0 otherwise 1
+                Time.timeScale = (isPaused) ? 0 : 1;
+
+                pauseMenu.SetActive(isPaused);
+            }
+            else
+            {
+                OpenPauseMenu();
+            }
+            
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void RestartGame()
+    {
+        Application.LoadLevel(Application.loadedLevelName);
+    }
+
+
+    public void IncreaseQuality()
+    {
+        QualitySettings.IncreaseLevel();
+        UpdateQualityLabel();
+    }
+
+    public void DecreaseQuality()
+    {
+        QualitySettings.DecreaseLevel();
+        UpdateQualityLabel();
+    }
+
+    public void SetVolume(float value)
+    {
+        AudioListener.volume = value;
+        UpdateVolumeLabel();
+    }
+
+    private void UpdateQualityLabel()
+    {
+        int currentQuality = QualitySettings.GetQualityLevel();
+        string qualityName = QualitySettings.names[currentQuality];
+
+        optionsMenu.transform.FindChild("Quality Level").GetComponent<UnityEngine.UI.Text>().text = "Quality Level - " +
+                                                                                                    qualityName;
+    }
+
+    private void UpdateVolumeLabel()
+    {
+        optionsMenu.transform.FindChild("Master Volume").GetComponent<UnityEngine.UI.Text>().text = "Master Volume - " +
+                                                                                                    (AudioListener.volume * 100).ToString("f2") + "%";
+    }
+
+    public void OpenOptions()
+    {
+        optionsMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    public void OpenPauseMenu()
+    {
+        optionsMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+}
