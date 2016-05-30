d9325540e5e85e8294dc68ba1be127e6e714ff93
Vishal Mewada <vishalm.packtpub@gmail.com>
2016-05-30 16:33:45 +0530

    Chapter 9 Code Files Added

---
+++ Chapter 9 Example Code/First Person Shooter/Assets/EnemyBehaviour.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public enum State
+    {
+        Idle,
+        Follow,
+        Die,
+    }
+
+    // The current state the player is in
+    public State state;
+
+    // The object the enemy wants to follow
+    public Transform target;
+
+    // How fast should the enemy move?
+    public float moveSpeed = 3.0f;
+    public float rotateSpeed = 3.0f;
+
+    // How close should the enemy be before they follow?
+    public float followRange = 10.0f;
+
+    // How far should the target be before the enemy gives up 
+    // following? 
+    // Note: Needs to be >= followRange
+    public float idleRange = 10.0f;
+
+    public float health = 100.0f;
+    private float currentHealth;
+
+
+    IEnumerator IdleState()
+    {
+        //OnEnter
+        Debug.Log("Idle: Enter");
+        while (state == State.Idle)
+        {
+            //OnUpdate
+            if (GetDistance() < followRange)
+            {
+                state = State.Follow;
+            }
+
+            yield return 0;
+        }
+        //OnEnd
+        Debug.Log("Idle: Exit");
+        GoToNextState();
+    }
+
+    IEnumerator FollowState()
+    {
+        Debug.Log("Follow: Enter");
+        while (state == State.Follow)
+        {
+            transform.position =
+      Vector3.MoveTowards(transform.position,
+                          target.position,
+                          Time.deltaTime * moveSpeed);
+
+            RotateTowardsTarget();
+
+            if (GetDistance() > idleRange)
+            {
+                state = State.Idle;
+            }
+
+            yield return 0;
+        }
+        Debug.Log("Follow: Exit");
+        GoToNextState();
+    }
+
+    IEnumerator DieState()
+    {
+        Debug.Log("Die: Enter");
+
+        Destroy(this.gameObject);
+        yield return 0;
+    }
+
+    public float GetDistance()
+    {
+        return (transform.position -
+    target.transform.position).magnitude;
+    }
+
+    private void RotateTowardsTarget()
+    {
+        transform.rotation =
+    Quaternion.Slerp(transform.rotation, 
+                Quaternion.LookRotation(target.position - 
+                                        transform.position),
+                               rotateSpeed * Time.deltaTime);
+    }
+
+    void GoToNextState()
+    {
+        // Find out the name of the function we want to call
+        string methodName = state.ToString() + "State";
+
+        // Searches this class for a function with the name of 
+        // state + State (for example: idleState)
+        System.Reflection.MethodInfo info =
+        GetType().GetMethod(methodName,
+        System.Reflection.BindingFlags.NonPublic |
+        System.Reflection.BindingFlags.Instance);
+        StartCoroutine((IEnumerator)info.Invoke(this, null));
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        GoToNextState();
+        currentHealth = health;
+    }
+
+    public void TakeDamage()
+    {
+        // The closer I am, the more damage I do
+        float damageToDo = 100.0f - (GetDistance() * 5);
+
+        if (damageToDo < 0)
+            damageToDo = 0;
+        if (damageToDo > health)
+            damageToDo = health;
+
+        currentHealth -= damageToDo;
+
+        if (currentHealth <= 0)
+        {
+            state = State.Die;
+        }
+        else
+        {
+            // If we're not dead, now that we took a picture the 
+            // enemy 
+            // knows where we are
+            followRange = Mathf.Max(GetDistance(), followRange);
+            state = State.Follow;
+        }
+
+        print("Ow! - Current Health: " +
+    currentHealth.ToString());
+
+    }
+
+
+}
---
+++ Chapter 9 Example Code/First Person Shooter/Assets/PhoneBehaviour.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections; // IEnumerator
+using UnityEngine.UI; // Image
+using System.Collections.Generic; // List
+
+public class PhoneBehaviour : MonoBehaviour
+{
+
+    public List<GameObject> phoneObjects;
+
+    public Image cameraFlash;
+
+    private bool cameraActive = false;
+
+    private bool shotStarted = false;
+
+    void Start()
+    {
+        SetCameraActive(false);
+    }
+
+    IEnumerator CameraFlash()
+    {
+        yield return StartCoroutine(Fade(0.0f, 0.8f, 0.2f,
+    cameraFlash));
+        yield return StartCoroutine(Fade(0.8f, 0.0f, 0.2f,
+    cameraFlash));
+        StopCoroutine("CameraFlash");
+    }
+
+
+    IEnumerator Fade(float start, float end, float length,
+                  UnityEngine.UI.Image currentObject)
+    {
+        if (currentObject.color.a == start)
+        {
+            Color curColor;
+            for (float i = 0.0f; i < 1.0f; 
+                 i += Time.deltaTime * (1 / length))
+            {
+                // Cannot modify the color property directly, so we 
+                // need to create a copy
+                curColor = currentObject.color;
+
+                // Do a linear interpolation of the value of the
+                // transparency from the start 
+                // value to the end value in equal increments
+                curColor.a = Mathf.Lerp(start, end, i);
+
+                // Then we assign the copy to the original object
+                currentObject.color = curColor;
+
+                yield return null;
+            }
+            curColor = currentObject.color;
+
+            // ensure the fade is completely finished (because 
+            // lerp doesn't always end on the exact value due to 
+            // rounding errors)
+            curColor.a = end;
+            currentObject.color = curColor;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        // Are we holding down the right mouse button
+        if ((Input.GetMouseButton(1) || (Input.GetAxis("360 Left Trigger") > 0)) && !cameraActive)
+        {
+            SetCameraActive(true);
+        }
+        else if(cameraActive && !(Input.GetMouseButton(1) || (Input.GetAxis("360 Left Trigger") > 0)))
+        {
+            SetCameraActive(false);
+        }
+
+        if (cameraActive && (Input.GetMouseButton(0) || (Input.GetAxis("360 Right Trigger") > 0)))
+        {
+            shotStarted = true;
+
+            StartCoroutine(CameraFlash());
+
+            GameObject[] enemyList = GameObject.FindGameObjectsWithTag("Enemy");
+
+            foreach (GameObject enemy in enemyList)
+            {
+                if (enemy.activeInHierarchy)
+                {
+                    EnemyBehaviour behaviour = enemy.GetComponent<EnemyBehaviour>();
+                    behaviour.TakeDamage();
+                }
+            }
+
+        }
+
+        if (Input.GetAxis("360 Right Trigger") == 0)
+        {
+            shotStarted = false;
+        }
+
+
+    }
+
+    void SetCameraActive(bool active)
+    {
+        cameraActive = active;
+
+        foreach (var obj in phoneObjects)
+        {
+            obj.SetActive(active);
+        }
+    }
+}
