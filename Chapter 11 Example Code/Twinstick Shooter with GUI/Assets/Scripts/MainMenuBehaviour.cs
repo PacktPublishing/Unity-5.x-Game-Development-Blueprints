ef29f7b00903366b3fada1f61e1fd4abe55da2de
Vishal Mewada <vishalm@packtpub.com>
2016-05-30 17:43:11 +0530

    Chapter 11 Code Files Added

---
+++ Chapter 11 Example Code/Twinstick Shooter with GUI/Assets/Scripts/MainMenuBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MainMenuBehaviour : MonoBehaviour {
+
+    public void LoadLevel(string levelName)
+    {
+        Application.LoadLevel(levelName);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
