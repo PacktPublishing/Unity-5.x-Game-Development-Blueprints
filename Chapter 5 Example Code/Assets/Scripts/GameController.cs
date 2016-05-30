84f1968fd8c80938de1339dde4b1dfb6cc0ba22b
Vishal Mewada <vishalm.packtpub@gmail.com>
2016-05-30 15:56:31 +0530

    Chapter 5 Code Files Added

---
+++ Chapter 5 Example Code/Assets/Scripts/GameController.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections; // IEnumerator
+using System.Collections.Generic; // List
+using UnityEngine.UI; // Text
+
+public class GameController : MonoBehaviour
+{
+    public static GameController _instance;
+
+    private float timeLeft;
+    public Text timeText;
+
+    private int score;
+    public Text scoreText;
+    public Text highScoreText;
+
+    //private int score;
+
+    [HideInInspector] // Hides var below
+    public List<TargetBehaviour> targets = new List<TargetBehaviour>();
+
+    void Awake()
+    {
+        _instance = this;
+        //score = 0;
+        timeLeft = 50;
+        timeText.text = timeLeft.ToString();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash(
+                        "from", timeLeft,
+                        "to", 0,
+                        "time", timeLeft,
+                        "onupdatetarget", gameObject,
+                        "onupdate", "tweenUpdate",
+                        "oncomplete", "GameComplete"
+                        ));
+        StartCoroutine("SpawnTargets");
+
+        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("highScore").ToString();
+        score = 0;
+    }
+
+    void GameComplete()
+    {
+        StopCoroutine("SpawnTargets");
+        timeText.color = Color.black;
+        timeText.text = "GAME OVER";
+    }
+
+    void tweenUpdate(float newValue)
+    {
+        timeLeft = newValue;
+        if (timeLeft > 10)
+        {
+            timeText.text = timeLeft.ToString("#");
+        }
+        else
+        {
+            timeText.color = Color.red;
+            timeText.text = timeLeft.ToString("#.0");
+        }
+    }
+
+    void SpawnTarget()
+    {
+        // Get a random target
+        int index = Random.Range(0, targets.Count);
+        TargetBehaviour target = targets[index];
+
+        // Show it
+        target.ShowTarget();
+    }
+
+    IEnumerator SpawnTargets()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        // Continue forever
+        while (true)
+        {
+            int numOfTargets = Random.Range(1, 4);
+
+            for (int i = 0; i < numOfTargets; i++)
+            {
+                SpawnTarget();
+            }
+
+            yield return new WaitForSeconds(Random.Range(0.5f * numOfTargets, 2.5f));
+        }
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+
+        if (score > PlayerPrefs.GetInt("highScore"))
+        {
+            PlayerPrefs.SetInt("highScore", score);
+            highScoreText.text = "High Score: " + score.ToString();
+        }
+    }
+}
---
+++ Chapter 5 Example Code/Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetBehaviour : MonoBehaviour {
+
+    private bool beenHit = false;
+    private Animator animator;
+
+
+    private bool activated;
+    private Vector3 originalPos;
+
+    private GameObject parent;
+
+
+    void Start()
+    {
+        parent = transform.parent.gameObject;
+        animator = parent.GetComponent<Animator>();
+        originalPos = parent.transform.position;
+        //ShowTarget();
+        GameController._instance.targets.Add(this);
+    }
+
+    public void ShowTarget()
+    {
+        if (!activated)
+        {
+            activated = true;
+            beenHit = false;
+            animator.Play("Idle");
+
+            iTween.MoveBy(parent, iTween.Hash("y", 1.4,
+                                              "easeType", "easeInOutExpo",
+                                              "time", 0.5,
+                                              "oncomplete", "OnShown",
+                                              "oncompletetarget", gameObject));
+        }
+
+    }
+
+    public IEnumerator HideTarget()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        // Move down to the original spot
+        iTween.MoveBy(parent.gameObject, iTween.Hash("y", (originalPos.y - 
+                                              parent.transform.position.y),
+                                              "easeType", "easeOutQuad",
+                                              "loopType", "none",
+                                              "time", 0.5,
+                                              "oncomplete", "OnHidden",
+                                              "oncompletetarget", gameObject));
+    }
+
+    /// <summary>
+	/// After the tween finishes, we now make sure we can be shown again.
+	/// </summary>
+	void OnHidden()
+    {
+        //Just to make sure the object's position resets
+        parent.transform.position = originalPos;
+        activated = false;
+    }
+
+    void OnShown()
+    {
+        StartCoroutine("MoveTarget");
+    }
+
+    public float moveSpeed = 1f;    // How fast to move in x axis
+    public float frequency = 5f;    // Speed of sine movement
+    public float magnitude = 0.1f;  // Size of sine movement
+
+    IEnumerator MoveTarget()
+    {
+        var relativeEndPos = parent.transform.position;
+
+        // Are we facing right or left?
+        if (transform.eulerAngles == Vector3.zero)
+        {
+            // if we're going right positive
+            relativeEndPos.x = 6;
+        }
+        else
+        {
+            // otherwise negative
+            relativeEndPos.x = -6;
+        }    
+
+        var movementTime = Vector3.Distance(parent.transform.position, relativeEndPos) * moveSpeed;
+
+        var pos = parent.transform.position;
+        var time = 0f;
+
+        while (time < movementTime)
+        {
+            time += Time.deltaTime;
+
+            pos += parent.transform.right * Time.deltaTime * moveSpeed;
+            parent.transform.position = pos + (parent.transform.up * 
+                Mathf.Sin(Time.time * frequency) * magnitude);
+
+            yield return new WaitForSeconds(0);
+        }
+
+        StartCoroutine(HideTarget());
+    }
+
+
+
+    /// <summary>
+    /// Called whenever the player clicks on the object. Only works if you 
+    /// have a collider
+    /// </summary>
+    void OnMouseDown()
+    {
+        // Is it valid to hit it
+        if (!beenHit && activated)
+        {
+            GameController._instance.IncreaseScore();
+            beenHit = true;
+            animator.Play("Flip");
+
+            StopAllCoroutines();
+
+            StartCoroutine(HideTarget());
+        }
+    }
+}
