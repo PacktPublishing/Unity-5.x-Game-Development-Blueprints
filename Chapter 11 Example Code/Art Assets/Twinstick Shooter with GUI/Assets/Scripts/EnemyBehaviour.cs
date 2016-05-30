eaa4fcb0476a55c251a5b17f1e5dc0177e32eeed
Vishal Mewada <vishalm@packtpub.com>
2016-05-30 17:38:54 +0530

    Chapter 11 Code Files Added

---
+++ Chapter 11 Example Code/Art Assets/Twinstick Shooter with GUI/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine; // MonoBehaviour
+
+public class EnemyBehaviour : MonoBehaviour 
+{
+	// How many times should enemy get hit before it dies
+	public int health = 2;
+
+	// When the enemy dies, we play an explosion
+	public Transform explosion;
+
+	// What sound to play when hit
+	public AudioClip hitSound;
+
+	
+	void OnCollisionEnter2D(Collision2D theCollision)
+	{
+		// Uncomment this line to check for collision
+		//Debug.Log("Hit"+ theCollision.gameObject.name);
+		
+		// this line looks for "Laser" in the name of 
+		// the object we collided with.
+		if(theCollision.gameObject.name.Contains("Laser"))
+		{
+			LaserBehaviour laser = theCollision.gameObject.GetComponent<LaserBehaviour>();
+			health -= laser.damage;
+			Destroy (theCollision.gameObject);
+
+			// Plays a sound from this object's AudioSource
+			GetComponent<AudioSource>().PlayOneShot(hitSound);
+		}
+		
+		if (health <= 0) 
+		{
+			Destroy (this.gameObject);
+
+			GameController controller = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+			controller.KilledEnemy();
+			controller.IncreaseScore(10);
+
+			// Check if explosion was set
+			if(explosion)
+			{
+				GameObject exploder = ((Transform)Instantiate(explosion, this.transform.position, this.transform.rotation)).gameObject;
+
+				Destroy(exploder, 2.0f);
+			}
+
+
+		}
+	}
+}
---
+++ Chapter 11 Example Code/Art Assets/Twinstick Shooter with GUI/Assets/Scripts/GameController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	// A reference to the enemy we wish to spawn
+	public Transform enemy;
+
+	[Header("Wave Properties")]
+
+	// We want to delay our code at certain times
+	public float timeBeforeSpawning = 1.5f;
+	public float timeBetweenEnemies = .25f;
+	public float timeBeforeWaves = 2.0f;
+	
+	public  int enemiesPerWave = 10;
+	private int currentNumberOfEnemies = 0;
+
+	[Header("User Interface")]
+	// The values we'll be printing
+	private int score = 0;
+	private int waveNumber = 0;
+	
+	// The actual GUI text objects
+	public Text scoreText;
+	public Text waveText;
+
+	public void IncreaseScore(int increase)
+	{
+		score += increase;
+		scoreText.text = "Score: " + score;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SpawnEnemies());
+	}
+
+	// Coroutine used to spawn enemies
+	IEnumerator SpawnEnemies()
+	{
+		// Give the player time before we start the game
+		yield return new WaitForSeconds(timeBeforeSpawning);
+		
+		// After timeBeforeSpawning has elapsed, we will enter 
+		// this loop
+		while(true)
+		{
+			// Don't spawn anything new until all of the previous
+			// wave's enemies are dead
+			if(currentNumberOfEnemies <= 0) 
+			{
+				waveNumber++;
+				waveText.text = "Wave: " + waveNumber;
+
+				//Spawn enemies in a random position
+				for (int i = 0; i < enemiesPerWave; i++) 
+				{
+					// We want the enemies to be off screen
+					// (Random.Range gives us a number between the 
+					// first and second parameter)
+					float randDistance = Random.Range(10, 25);
+					
+					// Enemies can come from any direction
+					Vector2 randDirection = 
+						              Random.insideUnitCircle;
+
+					Vector3 enemyPos = this.transform.position;
+					enemyPos.x += randDirection.x * randDistance;
+					enemyPos.y += randDirection.y * randDistance;
+					
+					// Spawn the enemy and increment the number of 
+					// enemies spawned 
+					// (Instantiate Makes a clone of the first 
+					// parameter and places it at the second with a 
+					// rotation of the third.)
+					Instantiate(enemy, enemyPos, 
+					            this.transform.rotation);
+					currentNumberOfEnemies++;
+					yield return new 
+						WaitForSeconds(timeBetweenEnemies);
+				}
+			}
+			// How much time to wait before checking if we need 
+			// to spawn another wave
+			yield return new WaitForSeconds(timeBeforeWaves);
+		}
+	}
+
+	// Allows classes outside of GameController to say when we 
+	// killed an enemy.
+	public void KilledEnemy()
+	{
+		currentNumberOfEnemies--;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
